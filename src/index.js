e4d1d4a0f1b7071a0c0bb13aefd4ab15d9c09448
Silvia Petruta <silvia_petruta@yahoo.com>
2020-08-19 10:52:42 +0100

    #4 Updated the method to check which screen type was selected.

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ const screenChoise = document.getElementById('screenChoice');
 let screenType = '';
 
 screenChoise.addEventListener('change', () => {
-    console.log(screenChoise.value);
-    if (screenChoise.value == 'screen1-2d') {
+     checkScreenType(screenChoise.value);
+});
+
+checkScreenType = (screen) => {
+    if (screen == 'screen1-2d') {
         screenType = '2d';
-    } else if (screenChoise.value == 'screen2-3d') {
+    } else if (screen == 'screen2-3d') {
         screenType = '3d';
-    } else if (screenChoise.value == 'screen3-2d') {
+    } else if (screen == 'screen3-2d') {
         screenType = '2d';
-    } else if (screenChoise.value == 'screen3-3d') {
+    } else if (screen == 'screen3-3d') {
         screenType = '3d';
     }
-});
+};
